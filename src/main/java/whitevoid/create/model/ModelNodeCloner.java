24b69165d3d0a9afc7d6c2fb3c8ea5d8f04e0d27
b2dgwjcyzd-dotcom <b2dgwjcyzd@privaterelay.appleid.com>
2026-09-20 18:39:38 +0200

    CREATE: add undoable geometry editing commands

---
+++ src/main/java/whitevoid/create/model/ModelNodeCloner.java
@@ -0,0 +1,22 @@
+package whitevoid.create.model;
+
+public final class ModelNodeCloner {
+    private ModelNodeCloner() {}
+
+    public static ModelNode deepCopy(ModelNode source) {
+        ModelNode copy = new ModelNode(source.name());
+        copy.setGeometry(source.geometry());
+
+        copy.transform().position(
+                source.transform().x(), source.transform().y(), source.transform().z());
+        copy.transform().rotation(
+                source.transform().rotationX(), source.transform().rotationY(), source.transform().rotationZ());
+        copy.transform().scale(
+                source.transform().scaleX(), source.transform().scaleY(), source.transform().scaleZ());
+
+        for (ModelNode child : source.children()) {
+            copy.addChild(deepCopy(child));
+        }
+        return copy;
+    }
+}
