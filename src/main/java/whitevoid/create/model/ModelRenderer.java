1ffa9c10f6f86d5930b6bbe133d9f9711b1984e1
b2dgwjcyzd-dotcom <b2dgwjcyzd@privaterelay.appleid.com>
2026-09-20 18:51:58 +0200

    CREATE: render editable mesh topology

--- src/main/java/whitevoid/create/model/ModelRenderer.java
+++ src/main/java/whitevoid/create/model/ModelRenderer.java
@@ -1,6 +1,9 @@
 package whitevoid.create.model;
 
 import java.util.function.BiConsumer;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 public final class ModelRenderer {
     private static final int[][] EDGES = {
@@ -31,6 +34,33 @@ public final class ModelRenderer {
 
     private TransformMath.Point corner(double x, double y, double z, ModelNode node) {
         return TransformMath.applyHierarchy(new TransformMath.Point(x, y, z), node);
+    }
+
+    public void renderMesh(Model model, BiConsumer<ModelNode, MeshGeometry> meshConsumer) {
+        for (ModelNode node : model.allNodes()) {
+            MeshGeometry mesh = node.meshGeometry();
+            if (mesh == null && node.geometry() != null) {
+                mesh = node.ensureMeshGeometry();
+            }
+            if (mesh != null) meshConsumer.accept(node, mesh);
+        }
+    }
+
+    public static int[][] meshEdges(MeshGeometry mesh) {
+        Set<Long> unique = new HashSet<>();
+        ArrayList<int[]> result = new ArrayList<>();
+        for (MeshGeometry.Face face : mesh.faces()) {
+            int[] indices = face.vertices();
+            for (int i = 0; i < indices.length; i++) {
+                int a = indices[i];
+                int b = indices[(i + 1) % indices.length];
+                int lo = Math.min(a, b);
+                int hi = Math.max(a, b);
+                long key = ((long) lo << 32) | (hi & 0xffffffffL);
+                if (unique.add(key)) result.add(new int[]{a, b});
+            }
+        }
+        return result.toArray(new int[0][]);
     }
 
     public static int[][] edges() {
