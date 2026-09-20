2849fd53918d1857921e6aecc6c0d6abe21eb147
b2dgwjcyzd-dotcom <b2dgwjcyzd@privaterelay.appleid.com>
2026-09-20 18:08:21 +0200

    CREATE: add hierarchical model foundation

---
+++ src/main/java/whitevoid/create/model/Model.java
@@ -0,0 +1,26 @@
+package whitevoid.create.model;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
+public final class Model {
+    private final ModelNode root = new ModelNode("Root");
+
+    public ModelNode root() {
+        return root;
+    }
+
+    public List<ModelNode> allNodes() {
+        List<ModelNode> nodes = new ArrayList<>();
+        root.collect(nodes);
+        return Collections.unmodifiableList(nodes);
+    }
+
+    public ModelNode addNode(String name) {
+        ModelNode node = new ModelNode(Objects.requireNonNull(name, "name"));
+        root.addChild(node);
+        return node;
+    }
+}
