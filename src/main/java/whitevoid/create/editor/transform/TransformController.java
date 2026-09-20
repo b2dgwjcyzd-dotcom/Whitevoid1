c9629ed20feb70dbdffdf9511c1b2f1a3ae1477c
b2dgwjcyzd-dotcom <b2dgwjcyzd@privaterelay.appleid.com>
2026-09-20 18:12:09 +0200

    CREATE: add incremental transform controls

--- src/main/java/whitevoid/create/editor/transform/TransformController.java
+++ src/main/java/whitevoid/create/editor/transform/TransformController.java
@@ -1,7 +1,6 @@
 package whitevoid.create.editor.transform;
 
 import whitevoid.create.model.ModelNode;
-import whitevoid.create.model.Transform;
 
 public final class TransformController {
     private TransformMode mode = TransformMode.SELECT;
@@ -12,18 +11,27 @@ public final class TransformController {
         this.mode = mode;
     }
 
-    public void move(ModelNode node, double x, double y, double z) {
-        Transform t = node.transform();
-        t.position(x, y, z);
+    public void translate(ModelNode node, double dx, double dy, double dz) {
+        node.transform().position(
+                node.transform().x() + dx,
+                node.transform().y() + dy,
+                node.transform().z() + dz
+        );
     }
 
-    public void rotate(ModelNode node, double x, double y, double z) {
-        Transform t = node.transform();
-        t.rotation(x, y, z);
+    public void rotateBy(ModelNode node, double dx, double dy, double dz) {
+        node.transform().rotation(
+                node.transform().rotationX() + dx,
+                node.transform().rotationY() + dy,
+                node.transform().rotationZ() + dz
+        );
     }
 
-    public void scale(ModelNode node, double x, double y, double z) {
-        Transform t = node.transform();
-        t.scale(x, y, z);
+    public void scaleBy(ModelNode node, double dx, double dy, double dz) {
+        node.transform().scale(
+                Math.max(0.01, node.transform().scaleX() + dx),
+                Math.max(0.01, node.transform().scaleY() + dy),
+                Math.max(0.01, node.transform().scaleZ() + dz)
+        );
     }
 }
