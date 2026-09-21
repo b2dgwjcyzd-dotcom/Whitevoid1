88c77002234b45fa5924b97cfd590d0e3de75ac1
b2dgwjcyzd-dotcom <b2dgwjcyzd@privaterelay.appleid.com>
2026-09-21 13:52:16 +0200

    CREATE: centralize gizmo drag completion

--- src/main/java/whitevoid/create/ui/CreateTransformGizmoInteractionController.java
+++ src/main/java/whitevoid/create/ui/CreateTransformGizmoInteractionController.java
@@ -1,6 +1,8 @@
 package whitevoid.create.ui;
 
 import whitevoid.create.core.CreateCore;
+import whitevoid.create.core.history.commands.ResizeCubeFaceCommand;
+import whitevoid.create.core.history.commands.SetTransformCommand;
 import whitevoid.create.editor.transform.TransformMode;
 import whitevoid.create.editor.viewport.ViewportContext;
 import whitevoid.create.model.ModelNode;
@@ -48,4 +50,60 @@ final class CreateTransformGizmoInteractionController {
         interaction.dragOldSz = transform.scaleZ();
         return true;
     }
+    boolean finish(CreateCore core, CreateViewportInteractionState interaction, ModelNode node, TransformMode mode) {
+        if (!interaction.gizmoDragging) return false;
+
+        if (node != null) {
+            if (mode == TransformMode.GEOMETRY
+                    && interaction.dragOldGeometry != null && node.geometry() != null) {
+                var transform = node.transform();
+                boolean geometryChanged = !interaction.dragOldGeometry.equals(node.geometry());
+                boolean positionChanged = interaction.dragOldX != transform.x()
+                        || interaction.dragOldY != transform.y()
+                        || interaction.dragOldZ != transform.z();
+                if (geometryChanged || positionChanged) {
+                    core.editorContext().history().recordExecuted(
+                            new ResizeCubeFaceCommand(
+                                    node,
+                                    interaction.dragOldGeometry,
+                                    node.geometry(),
+                                    interaction.dragOldX, interaction.dragOldY, interaction.dragOldZ,
+                                    transform.x(), transform.y(), transform.z()
+                            )
+                    );
+                }
+                interaction.dragOldGeometry = null;
+                interaction.gizmoDragging = false;
+                interaction.activeAxis = ViewportGizmo.Axis.NONE;
+                return true;
+            }
+
+            var transform = node.transform();
+            boolean changed = interaction.dragOldX != transform.x()
+                    || interaction.dragOldY != transform.y()
+                    || interaction.dragOldZ != transform.z()
+                    || interaction.dragOldRx != transform.rotationX()
+                    || interaction.dragOldRy != transform.rotationY()
+                    || interaction.dragOldRz != transform.rotationZ()
+                    || interaction.dragOldSx != transform.scaleX()
+                    || interaction.dragOldSy != transform.scaleY()
+                    || interaction.dragOldSz != transform.scaleZ();
+            if (changed) {
+                core.editorContext().history().recordExecuted(new SetTransformCommand(
+                        node,
+                        interaction.dragOldX, interaction.dragOldY, interaction.dragOldZ,
+                        interaction.dragOldRx, interaction.dragOldRy, interaction.dragOldRz,
+                        interaction.dragOldSx, interaction.dragOldSy, interaction.dragOldSz,
+                        transform.x(), transform.y(), transform.z(),
+                        transform.rotationX(), transform.rotationY(), transform.rotationZ(),
+                        transform.scaleX(), transform.scaleY(), transform.scaleZ(), true
+                ));
+            }
+        }
+
+        interaction.gizmoDragging = false;
+        interaction.activeAxis = ViewportGizmo.Axis.NONE;
+        return true;
+    }
+
 }
