ca4922e64dfe2613906bbc2914c4978c9f60d95c
b2dgwjcyzd-dotcom <b2dgwjcyzd@privaterelay.appleid.com>
2026-09-21 14:31:01 +0200

    Attach editable model data to CREATE projects

--- src/main/java/whitevoid/create/project/CreateProject.java
+++ src/main/java/whitevoid/create/project/CreateProject.java
@@ -1,16 +1,32 @@
 package whitevoid.create.project;
 
 import java.util.Objects;
+import whitevoid.create.model.Model;
 
 public final class CreateProject {
     private ProjectMetadata metadata;
+    private Model model;
 
     public CreateProject(ProjectMetadata metadata) {
+        this(metadata, Model.withDefaultCube());
+    }
+
+    public CreateProject(ProjectMetadata metadata, Model model) {
         this.metadata = Objects.requireNonNull(metadata, "metadata");
+        this.model = Objects.requireNonNull(model, "model");
     }
 
     public ProjectMetadata metadata() {
         return metadata;
+    }
+
+    public Model model() {
+        return model;
+    }
+
+    public void setModel(Model model) {
+        this.model = Objects.requireNonNull(model, "model");
+        touch();
     }
 
     public void touch() {
